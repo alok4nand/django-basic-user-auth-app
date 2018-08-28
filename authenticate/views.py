e32e8b3a097e4251deb990633cdd0fbca226d9c3
Alok Anand <alok4nand@gmail.com>
2018-08-28 22:19:56 +0530

    Implemented User Registration

--- authenticate/views.py
+++ authenticate/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import SignUpForm
 
 # Create your views here.
 def home(request):
@@ -25,3 +27,21 @@ def logout_user(request):
     logout(request)
     messages.success(request, ('Logged out'))
     return redirect('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            messages.success(request, "Registration Successful.")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    
+    context = {'form' : form}  
+    return render(request, 'authenticate/register.html', context)
+  
